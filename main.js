d0d03b96eaecb5230bcf57ce56cc732a54540661
resqa <resul1965qurbanov@gmail.com>
2022-12-09 00:27:43 +0400

    cocacola sayt home task

---
+++ main.js
@@ -0,0 +1,14 @@
+
+const hamburger = document.getElementById('hamburgerid')
+const navbarfixed = document.querySelector(".navbar_fixed")
+const hamburgerX = document.getElementById("hamburgerX")
+hamburger.onclick = () => {
+    navbarfixed.style.display = "block"
+    hamburger.style.display = "none"
+    hamburgerX.style.display = "block"
+}
+hamburgerX.onclick = () => {
+    hamburgerX.style.display = "none"
+    hamburger.style.display = "block"
+    navbarfixed.style.display = "none"
+}
